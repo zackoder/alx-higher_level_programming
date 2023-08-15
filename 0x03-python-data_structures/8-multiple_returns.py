7dfcd2bc7e75c7b21e9a0eaeefdd60ba9a3ff92c
zackoder <besszakaria@gmail.com>
2023-08-15 13:18:41 -0700

    commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     myTuple = ()
     if len(sentence) == 0:
-        myTuple = 0, None
+        myTuple = 0, "None"
     else:
         myTuple = len(sentence), sentence[0]
         return myTuple
