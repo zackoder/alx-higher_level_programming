716e78095d2b8d07405ddeb95795b4d23af0c958
zackoder <besszakaria@gmail.com>
2023-08-28 13:06:53 -0700

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             n += 1
         except IndexError:
-            break
+            pass
     print()
     return n
