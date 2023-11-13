fc76bbe8409596e56b37b99d1395b6b49be901df
zackoder <besszakaria@gmail.com>
2023-11-13 11:56:45 -0800

    commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2] == undefined) {
+	console.log("No argument");
+} else if (process.argv[2] != undefined && process.argv[3] == undefined) {
+	console.log("Argument found");
+} else {
+	console.log("Arguments found");
+}
