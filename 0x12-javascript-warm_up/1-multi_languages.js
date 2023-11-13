0fe2bd9215703e30e9bde2ef023656cf9a89dafe
zackoder <besszakaria@gmail.com>
2023-11-13 12:39:22 -0800

    commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-function print(str) {
-	console.log(str);
-}
-print("C is fun");
-print("Python is cool");
-print("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
